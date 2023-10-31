2844fbb6ddd3e26a7104c93cdafd3cd71d38f6e9
ryfiii <mzk114.cube@gmail.com>
2023-10-31 16:04:54 +0900

    Chat画面実装

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <Chat v-if="user.name" @logout="sessionCheck" />
+    <Chat v-if="user.name" @logout="sessionCheck" :loginUser="user" />
     <Login v-else @login="sessionCheck" />
   </div>
 </template>
@@ -9,26 +9,31 @@
 import Login from "@/views/LoginView.vue"
 import Chat from "@/views/ChatView.vue"
 
-import { onMounted, reactive,} from "vue";
+import { onMounted, ref,} from "vue";
 import axios from "axios";
 
-let user = reactive({
+let user = ref({
+  id: null,
   name: null,
   avatar: null,
 })
 
+//セッション保有確認メソッド
 const sessionCheck = async () => {
   const res = await axios.get("http://localhost:3000/check-login-session", {withCredentials: true})
   if (res.data.user) {
-    user.name = res.data.user.name
-    user.avatar = res.data.user.avatar
+    user.value.id = res.data.user.id
+    user.value.name = res.data.user.name
+    user.value.avatar = res.data.user.avatar
   }else {
-    user.name = null
-    user.avatar = null
+    user.value.id = null
+    user.value.name = null
+    user.value.avatar = null
   }
-  console.log(user.name)
+  console.log(user)
 }
 
+//ページ表示時にセッションがあるか確認
 onMounted(() => {
   sessionCheck()
 })
--- frontend/src/views/ChatView.vue
+++ frontend/src/views/ChatView.vue
@@ -1,46 +1,78 @@
 <template>
-    <div>
-        <h1>Chat</h1>
-
-        <div v-for="chat in chats" :key="chat.id">
-            <div v-if="chat.name == session.name">
-                <div class="text-end">
-                    <p>{{ chat.name }}</p>
-                    <p>{{ chat.message }}</p>
-                </div>
+    <div class="p-4 flex flex-col space-y-4">
+        <div v-for="chat in chats" :key="chat.id" :class="{ 'text-right': chat.user.id === loginUser.id }">
+            <div v-if="chat.user.id === loginUser.id" class="flex flex-col items-end">
+                <p class="bg-green-500 text-white max-w-xs rounded-lg p-2">{{ chat.message }}</p>
+                <span class="text-xs text-gray-500">{{ chat.created_at }}</span>
+                <img :src="'http://localhost:3000/' + chat.user.avatar" alt="avatar" class="w-8 h-8 rounded-full mt-2">
             </div>
-            <div v-else>
-                <div class="text-start">
-                    <p>{{ chat.name }}</p>
-                    <p>{{ chat.message }}</p>
-                </div>
+            <div v-else class="flex flex-col items-start">
+                <p class="bg-gray-300 max-w-xs rounded-lg p-2">{{ chat.message }}</p>
+                <span class="text-xs text-gray-500">{{ chat.created_at }}</span>
+                <img :src="'http://localhost:3000/' + chat.user.avatar" alt="avatar" class="w-8 h-8 rounded-full mt-2">
             </div>
         </div>
 
-        <form action="" method="post">
-            <input type="text" name="chat">
-            <input type="submit" value="送信">
-        </form>
-
-        <button @click="logout" class="btn btn-primary">Logout</button>
+        <div class="flex justify-between items-center mt-4">
+            <input type="text" v-model="message" class="form-input flex-1 mr-4" placeholder="Type a message">
+            <button @click="sendChat" class="btn btn-primary">Send</button>
+        </div>
     </div>
+    <button @click="logout" class="btn btn-primary">Logout</button>
 </template>
 
-<script setup lang="ts">
-import axios from 'axios';
-import { reactive } from 'vue';
 
-let chats = reactive({})
+
+<script setup lang="ts">
+import axios from 'axios'
+import { ref, onMounted, onUnmounted } from 'vue'
+import io from "socket.io-client"
+
+//props
+const props = defineProps({
+    loginUser: Object,
+})
+
+//取得した全チャット格納
+const chats = ref<any>([])
+
+//入力される文字格納
+const message = ref("")
+
+//エミット
 const emit = defineEmits()
 
+//関数
 const getChats = async () => {
     const res = await axios.get("http://localhost:3000/get-chats")
-    chats = res.data
+    chats.value = res.data
+    console.log(res.data)
+}
+
+const sendChat = async () => {
+    const res = await axios.post("http://localhost:3000/post-chat", { message: message.value }, { withCredentials: true })
+    message.value = ""
+    console.log(res.data)
 }
 
 const logout = async () => {
-  const res = await axios.get("http://localhost:3000/delete-session", {withCredentials: true})
-  console.log(res)
-  emit("logout")
+    const res = await axios.get("http://localhost:3000/delete-session", { withCredentials: true })
+    console.log(res)
+    emit("logout")
 }
+
+//socket.io-clientの設定
+const socket = io("http://localhost:3000")
+
+onMounted(() => {
+    getChats()
+
+    socket.on("chat message", (messages) => {
+        chats.value = messages
+    })
+}),
+
+    onUnmounted(() => {
+        socket.disconnect()
+    })
 </script>
