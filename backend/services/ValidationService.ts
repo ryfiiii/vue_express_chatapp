d6d2e93c3cdb5ffd5688b800515ddfef287b5995
ryfiii <mzk114.cube@gmail.com>
2023-11-01 17:07:16 +0900

    fixed validate

--- backend/services/ValidationService.ts
+++ backend/services/ValidationService.ts
@@ -2,10 +2,10 @@ import { Request, Response } from "express"
 
 class ValidationService {
 
-    static userValidate(req: Request, res: Response){
+    static userValidate(req: Request, res: Response): boolean{
 
         const username = req.body.username
-        const usernamePattern = /^[A-Za-z0-9\u3040-\u309F\u30A0-\u30FF\uFF00-\uFF9F！？!?]{1,20}$/
+        const usernamePattern = /^[A-Za-z0-9\u3040-\u309F\u30A0-\u30FF\uFF00-\uFF9F]{1,20}$/
 
         if(username == "" || !usernamePattern.test(username)){
             return false
@@ -14,7 +14,7 @@ class ValidationService {
         return true
     }
 
-    static chatValidate(chat: string){
+    static chatValidate(chat: string): boolean{
 
         const chatPattern = /^[A-Za-z0-9\u3040-\u309F\u30A0-\u30FF\uFF00-\uFF9F！？!?]{1,250}$/
 
